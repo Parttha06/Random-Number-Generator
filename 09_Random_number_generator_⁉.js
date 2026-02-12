72b0c760a2125d5cc7698f919a8425b4ea0c85aa
Partha Biswas <parthabiswas.per@gmail.com>
2026-02-12 20:38:19 +0530

    Add files via upload

---
+++ 09_Random_number_generator_⁉.js
@@ -0,0 +1,25 @@
+// RANDOM NUMBER GENERATOR
+
+// let randomNum = Math.floor(Math.random() * maxNum) + minNum;
+// Math.random generates the random number. maxNum sets the higher limit
+// and minNum stes the lower limit 
+
+
+const myButton = document.getElementById("myButton");
+const myLabel1 = document.getElementById("myLabel1");
+const myLabel2 = document.getElementById("myLabel2");
+const myLabel3 = document.getElementById("myLabel3");
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function (){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    myLabel1.textContent = randomNum1;
+    myLabel2.textContent = randomNum2;
+    myLabel3.textContent = randomNum3;
+}
